380d48e339075399f2c44f455f6838f40429a7eb
LPettay <lpettay@paloaltonetworks.com>
2022-12-09 08:22:53 -0800

    Day4_part2 solution

--- Day4_part2.py
+++ Day4_part2.py
@@ -1,4 +1,4 @@
-# Day4_part1.py
+# Day4_part2.py
 # Lance Pettay
 # AdventOfCode2022
 
@@ -8,9 +8,9 @@ with open('Day4/input.txt', 'r') as puzzle_input:
 overlap_counter = 0
 for pair in assignments:
     elf1_sections, elf2_sections = ([int(s) for s in section.split('-')] for section in pair.split(','))
-    if all(sections in range(elf1_sections[0], elf1_sections[1]+1) for sections in range(elf2_sections[0], elf2_sections[1]+1)):
+    if any(sections in range(elf1_sections[0], elf1_sections[1]+1) for sections in range(elf2_sections[0], elf2_sections[1]+1)):
         overlap_counter += 1
-    elif all(sections in range(elf2_sections[0], elf2_sections[1]+1) for sections in range(elf1_sections[0], elf1_sections[1]+1)):
+    elif any(sections in range(elf2_sections[0], elf2_sections[1]+1) for sections in range(elf1_sections[0], elf1_sections[1]+1)):
         overlap_counter += 1
 
 print(overlap_counter)
